8970343dd55da82808bf1b128ae6e7f1c8f86465
Ali Yara <ali.yara.fr@gmail.com>
2024-11-01 15:48:50 +0100

    ajoute some items in task card

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
-const addTask = document.querySelector("#new__task");
-const closeModal = document.querySelector("#close__modal-btn");
-const addTaskModal = document.querySelector("#add__task-model--eff");
+const addTask = document.querySelector("#new__task"); // btn
+const closeModal = document.querySelector("#close__modal-btn"); // close modal icon
+const addTaskModal = document.querySelector("#add__task-model--eff"); // modal 
 const toggleEffect = document.querySelector(".toggle__effect");
 
 addTask.addEventListener("click", () => {
   addTaskModal.classList.add("active");
   toggleEffect.classList.add("active");
 });
+
 closeModal.addEventListener("click", () => {
   addTaskModal.classList.remove("active");
   toggleEffect.classList.remove("active");
@@ -38,16 +39,18 @@ form.addEventListener("submit", (e) => {
 
   // Add the new task to the tasks array
   tasksList.push(newTask);
-  this.reset();
   renderTasks();
 
-  // close the modal
-  addTaskModal.classList.remove("active");
-  toggleEffect.classList.remove("active");
+  // Reset form fields
+  form.reset();
 
-  // the todo cards length
+  // Update the todo cards length
   let todoLength = document.querySelector("#todo__length");
   todoLength.textContent = tasksList.length;
+
+  // Close the modal
+  addTaskModal.classList.remove("active");
+  toggleEffect.classList.remove("active");
 });
 
 function renderTasks() {
@@ -56,17 +59,51 @@ function renderTasks() {
 
   tasksList.forEach((task, index) => {
     const taskItem = document.createElement("div");
-    console.log(task.title);
     taskItem.className = "col-cards-card";
     taskItem.innerHTML = `
-                <div class="col__card-title">
-                  <div class="col__card-priority"></div>
-                  <h3>${task.taskTitle}</h3>
+                <div class="colm__card--top">
+                    <div class="col__card-title">
+                        <h3>${task.taskTitle}</h3>
+                    </div>
+                    <div class="col__card-edit">
+                        <i class="fa-solid fa-pen"></i>
+                    </div>
                 </div>
-                <div class="col__card-edit">
-                  <i class="fa-solid fa-pen"></i>
+                <div class="colm__card--middle">
+                    <div class="col__card-description">
+                        <p>${task.taskDecription}</p>
+                    </div>
+                </div>
+                <div class="colm__card-bottom">
+                    <div class="card__bottom-left">
+                        <div class="col__card-priority">
+                            <span>${task.taskPeriority}</span>
+                        </div>
+                        <div class="col__card-status">
+                            <span>${task.taskStatus}</span>
+                        </div>
+                    </div>
+                    <div class="card__bottom-right">
+                        <p>${task.taskDate}</p>
+                    </div>
+                    
                 </div>
         `;
+
+    // Set the priority color for each task card
+    const periorityColor = taskItem.querySelector(".col__card-priority");
+    switch (task.taskPeriority) {
+      case "High":
+        periorityColor.style.backgroundColor = "#1569f1";
+        break;
+      case "Medium":
+        periorityColor.style.backgroundColor = "#1cab06";
+        break;
+      case "Low":
+        periorityColor.style.backgroundColor = "#c8b105";
+        break;
+    }
+
     taskList.appendChild(taskItem);
   });
 }
